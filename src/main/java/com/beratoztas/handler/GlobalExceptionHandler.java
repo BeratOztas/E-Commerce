bf6fab967eff60c02f0961fcb997ddce92e1c98f
Berat <berat230@gmail.com>
2025-04-12 19:14:26 +0300

    feat: Add global validation handler for request validation

--- src/main/java/com/beratoztas/handler/GlobalExceptionHandler.java
+++ src/main/java/com/beratoztas/handler/GlobalExceptionHandler.java
@@ -2,10 +2,18 @@ package com.beratoztas.handler;
 
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -21,6 +29,22 @@ public class GlobalExceptionHandler {
 			return ResponseEntity.badRequest().body(createApiError(ex.getMessage(), request));
 		}
 
+		//Validation Exception Handler 
+		@ExceptionHandler(value = { MethodArgumentNotValidException.class })
+		public ResponseEntity<ApiError<Map<String, List<String>>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+			Map<String, List<String>> errorsMap = new HashMap<>();
+			
+			for (ObjectError objError : ex.getBindingResult().getAllErrors()) {
+				String fieldName = ((FieldError) objError).getField();
+				String message = objError.getDefaultMessage();
+				if (errorsMap.containsKey(fieldName))
+					errorsMap.get(fieldName).add(message);
+				else
+					errorsMap.put(fieldName, new ArrayList<>(Arrays.asList(message)));
+			}
+
+			return ResponseEntity.badRequest().body(createApiError(errorsMap, request));
+		}
 		public <E> ApiError<E> createApiError(E message, WebRequest request) {
 			ApiError<E> apiError = new ApiError<>();
 			apiError.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
